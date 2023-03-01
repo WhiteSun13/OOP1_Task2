424665e23ed7633a2c31d6f7df7ba97fc98f19d1
sabibulaev25@gmail.com <sabibulaev25@gmail.com>
2023-03-01 05:01:33 +0300

    Добавьте файлы проекта.

---
+++ OOP1_Task2/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace OOP1_Task2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxValue = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.runIteration = new System.Windows.Forms.Button();
+            this.amOfIter = new System.Windows.Forms.Label();
+            this.RootOnIter = new System.Windows.Forms.Label();
+            this.fault = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxValue
+            // 
+            this.textBoxValue.Location = new System.Drawing.Point(6, 41);
+            this.textBoxValue.Name = "textBoxValue";
+            this.textBoxValue.Size = new System.Drawing.Size(192, 27);
+            this.textBoxValue.TabIndex = 0;
+            this.textBoxValue.TextChanged += new System.EventHandler(this.textBoxValue_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(23, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "(.NET Framework)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(23, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "(Метод Ньютона)";
+            // 
+            // Calculate
+            // 
+            this.Calculate.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.Calculate.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Calculate.Location = new System.Drawing.Point(539, 165);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(105, 51);
+            this.Calculate.TabIndex = 3;
+            this.Calculate.Text = "Вычислить";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // runIteration
+            // 
+            this.runIteration.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.runIteration.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.runIteration.Location = new System.Drawing.Point(650, 165);
+            this.runIteration.Name = "runIteration";
+            this.runIteration.Size = new System.Drawing.Size(106, 51);
+            this.runIteration.TabIndex = 4;
+            this.runIteration.Text = "Выполнить интерацию";
+            this.runIteration.UseVisualStyleBackColor = true;
+            this.runIteration.Click += new System.EventHandler(this.runIteration_Click);
+            // 
+            // amOfIter
+            // 
+            this.amOfIter.AutoSize = true;
+            this.amOfIter.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.amOfIter.Location = new System.Drawing.Point(6, 23);
+            this.amOfIter.Name = "amOfIter";
+            this.amOfIter.Size = new System.Drawing.Size(85, 20);
+            this.amOfIter.TabIndex = 5;
+            this.amOfIter.Text = "Итераций:";
+            // 
+            // RootOnIter
+            // 
+            this.RootOnIter.AutoSize = true;
+            this.RootOnIter.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.RootOnIter.Location = new System.Drawing.Point(6, 52);
+            this.RootOnIter.Name = "RootOnIter";
+            this.RootOnIter.Size = new System.Drawing.Size(212, 20);
+            this.RootOnIter.TabIndex = 6;
+            this.RootOnIter.Text = "Корень во время итерации:";
+            // 
+            // fault
+            // 
+            this.fault.AutoSize = true;
+            this.fault.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.fault.Location = new System.Drawing.Point(6, 82);
+            this.fault.Name = "fault";
+            this.fault.Size = new System.Drawing.Size(109, 20);
+            this.fault.TabIndex = 7;
+            this.fault.Text = "Погрешность:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.textBoxValue);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(204, 110);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Введите число";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.Location = new System.Drawing.Point(222, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(534, 110);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Вычисление";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.groupBox3.Controls.Add(this.RootOnIter);
+            this.groupBox3.Controls.Add(this.fault);
+            this.groupBox3.Controls.Add(this.amOfIter);
+            this.groupBox3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox3.Location = new System.Drawing.Point(12, 128);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(516, 128);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Метод Ньютона:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(768, 263);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.runIteration);
+            this.Controls.Add(this.groupBox1);
+            this.MaximumSize = new System.Drawing.Size(786, 310);
+            this.MinimumSize = new System.Drawing.Size(786, 310);
+            this.Name = "Form1";
+            this.Text = "Задание 2";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.Button runIteration;
+        private System.Windows.Forms.Label amOfIter;
+        private System.Windows.Forms.Label RootOnIter;
+        private System.Windows.Forms.Label fault;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ OOP1_Task2/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace OOP1_Task2
+{
+    public partial class Form1 : Form
+    {
+        //Параметры для методов PreparationForIter() и Iteration()
+        long itr = 0;
+        decimal numDecItr;
+        decimal deltaItr;
+        decimal guessItr;
+        decimal resultItr;
+        //Расcчет корня через MathSqrt
+        /// <param name="numberDouble">Принимает число типа double</param>
+        /// <returns>Возвращает квадратный корень этого числа</returns>
+        double SqrtRoot(double numberDouble)
+        {
+            return Math.Sqrt(numberDouble);
+        }
+        //Нахождение корня через MathSqrt
+        void MathSqrt()
+        {
+            //Расчет по Math Sqrt
+            double numberDouble; //Тут будет хранится значение из textBox
+            double squareRoot; //Тут хранится значение корня, вычисленное при помощи Math.Sqrt
+            bool result1 = double.TryParse(textBoxValue.Text, out var number);//Проверям, ввели ли double
+            if (result1 == true)
+            {
+
+                if (number < 0) //Положителен ли он
+                {
+                    MessageBox.Show("Введите положительное число"); return;
+                }
+                else
+                {
+                    numberDouble = number; //Если да, то находим корень через MathSqrt
+                    squareRoot = SqrtRoot(numberDouble);
+                    label1.Text = squareRoot.ToString() + " (.NET Framework)";
+                    //Выводим это значение на label
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите double");
+                return;
+            }
+        }
+        //Нахождение корня методом Ньютона
+        void NewtonRoot()
+        {
+            //Проверяем ввод
+            bool result2 = decimal.TryParse(textBoxValue.Text, out var number2);
+            if (result2 == true)
+            {
+                if (number2 == 0)
+                {
+                    label2.Text = 0 + " (Метод Ньютона)";
+                    return;
+                }
+                if (number2 < 0) //Положителен ли он
+                {
+                    MessageBox.Show("Введите положительное число"); return;
+                }
+                decimal numberDecimal = number2;
+                decimal delta = (decimal)Math.Pow(10, -28);
+                //Самое маленькое значение поддерживаемое decimal. Нужно для проверки точности результата
+                decimal guess = (decimal)Math.Sqrt((double)numberDecimal / 2);
+                //В result хранятся значения, производимые на каждой итерации алгоритма
+                decimal result = ((numberDecimal / guess) + guess) / 2;//Основанные на предыдущих значениях
+                while (Math.Abs(result - guess) > delta)
+                {
+                    guess = result;
+                    result = ((numberDecimal / guess) + guess) / 2;
+                }
+                label2.Text = result.ToString() + " (Метод Ньютона)";
+
+            }
+            else
+            {
+                MessageBox.Show("Введите decimal"); return;
+            }
+        }
+        //Метод, задающий значения и проверяющий введенное значение на 0 и -
+        void PreparationForIter()
+        {
+            if (itr == 0)
+            {
+                bool resItr = decimal.TryParse(textBoxValue.Text, out var numItr);
+                if (resItr == true) //Проверка на ввод не текста
+                {
+                    if (numItr == 0) //Проверка на 0
+                    {
+                        label2.Text = numItr + " (Метод Ньютона)";
+                        amOfIter.Text = "Итераций: " + numItr;
+                        fault.Text = "Погрешность: " + numItr;
+                        RootOnIter.Text = "Корень во время итерации: " + numItr;
+                        itr = 0;
+                        return;
+                    }
+                    if (numItr < 0) //Положителен ли он
+                    {
+                        MessageBox.Show("Введите положительное число"); return;
+                    }
+                    numDecItr = numItr; //То же, что и в обычном методе Ньютона
+                    deltaItr = (decimal)Math.Pow(10, -28);
+                    guessItr = (decimal)Math.Sqrt((double)numDecItr / 2);
+                    resultItr = ((numDecItr / guessItr) + guessItr) / 2;
+                }
+                else
+                {
+                    MessageBox.Show("Введите decimal"); return;
+                }
+            }
+        }
+        //Одна итерация по нажатию на кнопку
+        void Iteration()
+        {
+            //То же, что и в методе Ньютона, но без цикла
+            if (Math.Abs(resultItr - guessItr) > deltaItr)
+            {
+                guessItr = resultItr;
+                resultItr = ((numDecItr / guessItr) + guessItr) / 2;
+            }
+            else
+            {
+                label2.Text = resultItr.ToString() + " (Метод Ньютона)";
+                itr = 0;
+                resultItr = 0;
+                guessItr = 0;
+                return;
+            }
+            itr++;
+            amOfIter.Text = "Количество итераций: " + itr;
+            fault.Text = "Погрешность: " + Math.Abs(resultItr - guessItr);
+            RootOnIter.Text = "Корень во время итерации: " + resultItr;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Нажатие на кнопку Вычислить
+        public void Calculate_Click(object sender, EventArgs e)
+        {
+            MathSqrt();
+            NewtonRoot();
+        }
+        //Нажатие на кнопку Выполнить итерацию
+        public void runIteration_Click(object sender, EventArgs e)
+        {
+            PreparationForIter();
+            Iteration();
+        }
+        //Изменение значения в textbox
+        private void textBoxValue_TextChanged(object sender, EventArgs e)
+        {
+            itr = 0;
+            label1.Text = "(.NET Framework)";
+            label2.Text = "(Метод Ньютона)";
+        }
+    }
+}
